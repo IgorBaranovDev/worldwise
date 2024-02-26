f831e7af4f28601a07dc8d49ec391ca8cf54fba9
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-02-26 16:29:10 +0100

    feat: created routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-function App() {  
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+
+function App() {
   return (
-    <div>
-      Worldwise
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

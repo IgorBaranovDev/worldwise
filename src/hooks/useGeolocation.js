7abadf4f36e3b61fa3ca704634028b885079b957
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-02-28 16:11:07 +0100

    feat: implemented deleting city

--- src/hooks/useGeolocation.js
+++ src/hooks/useGeolocation.js
@@ -5,8 +5,6 @@ export function useGeolocation(defaultPosition = null) {
   const [position, setPosition] = useState(defaultPosition);
   const [error, setError] = useState(null);
 
-  console.log("hook", position);
-
   function getPosition() {
     if (!navigator.geolocation)
       return setError("Your browser does not support geolocation");
